1fb8df532ed3a2724b20e416ec71b5adca14d6cf
luislopezgsn <luis.lopezg@sngular.com>
2025-01-20 11:25:55 +0100

    Finished lesson extracting text values

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -65,7 +65,7 @@ describe('First Test Suite', () => {
           .click({ force: true })
     })
 
-    it.only('Save the subject of the command', () => {
+    it('Save the subject of the command', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -92,5 +92,41 @@ describe('First Test Suite', () => {
 
 
     })  
+
+    it.only('extract text values', () => {
+      cy.visit('/')
+      cy.contains('Forms').click()
+      cy.contains('Form Layouts').click()
+
+      // 1  
+      cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+      
+      //2
+      cy.get('[for="exampleInputEmail1"]').then( label => {
+        const labelText = label.text()
+        expect(label.text()).to.equal('Email address')
+        cy.wrap(label).should('contain', 'Email address')
+      })
+
+      //3
+      cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+        expect(text).to.equal('Email address')
+      })
+
+      cy.get('[for="exampleInputEmail1"]').invoke('text').as('labelText').should('contain', 'Email address')
+
+      //4
+
+        cy.get('[for="exampleInputEmail1"]').invoke('attr', 'class').then( classValue => {
+          expect(classValue).to.equal('label')
+        })
+
+        //5 Invoke property
+        cy.get('#exampleInputEmail1').type('test@test.com')
+        cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com').then( property => {
+          expect(property).to.equal('test@test.com')
+        })
+
+    })
 
 })
