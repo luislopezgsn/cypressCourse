8656347d4a9b811db268b8aba39258e38250dcac
luislopezgsn <luis.lopezg@sngular.com>
2025-01-20 10:26:57 +0100

    Updated cypress, fixed cypress finished second lesson findind web elements

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -35,4 +35,32 @@ describe('First Test Suite', () => {
 
     })
 
+    it('Second Test', () => {
+      cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+       // Theory
+       // get() - find elements on the page by locator globally
+       // find() - find child elements by locator
+       // contains() - find HTML text and by text and locator
+
+
+
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.contains('nb-card', 'Horizontal form').find('button')
+        cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+        cy.contains('nb-card', 'Horizontal form').get('button')
+       
+        // Cypress chains and DOM
+        cy.get('#inputEmail3')
+          .parents('form')
+          .find('button')
+          .should('contain', 'Sign in')
+          .parents('form')
+          .find('nb-checkbox')
+          .click({ force: true })
+    })
+
 })
