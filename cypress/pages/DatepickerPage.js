5ef4974f6b7d7e654003a5cf5982235ff18775d8
luislopezgsn <luis.lopezg@sngular.com>
2025-02-25 13:12:30 +0100

    fixing tests WIP

--- cypress/pages/DatepickerPage.js
+++ cypress/pages/DatepickerPage.js
@@ -1,29 +1,37 @@
 class DatepickerPage {
-    selectFutureDate() {
+  selectFutureDate() {
       cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
-        cy.wrap(input).click();
-  
-        let date = new Date();
-        date.setDate(date.getDate() + 1);
-        let futureDay = date.getDate();
-        let futureMonth = date.toLocaleString('en-US', { month: 'short' });
-        let futureYear = date.getFullYear();
-        let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`;
-  
-        cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
-          if (!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)) {
-            cy.get('[data-name="chevron-right"]').click().then(() => {
-              this.selectFutureDate();
-            });
-          } else {
-            cy.get('day-cell').not('.bounding-month').contains(futureDay).click();
-          }
-        });
-  
-        cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert);
+          cy.wrap(input).scrollIntoView().click({ force: true });
+
+          let date = new Date();
+          date.setDate(date.getDate() + 1);
+          let futureDay = date.getDate();
+          let futureMonth = date.toLocaleString('en-US', { month: 'short' });
+          let futureYear = date.getFullYear();
+          let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`;
+
+          // Wait for the datepicker to fully load
+          cy.get('nb-calendar-day-picker', { timeout: 5000 }).should('be.visible');
+
+          cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+              if (!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)) {
+                  cy.get('[data-name="chevron-right"]').click().then(() => {
+                      this.selectFutureDate();
+                  });
+              } else {
+                  // Ensure day-cell is found before clicking
+                  cy.get('day-cell').then($cells => {
+                      if ($cells.length === 0) {
+                          throw new Error("No day-cell elements found!");
+                      }
+                  });
+                  cy.get('day-cell').should('exist').not('.bounding-month').contains(futureDay).click();
+              }
+          });
+
+          cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert);
       });
-    }
   }
-  
-  export default DatepickerPage;
-  
+}
+
+export default DatepickerPage;
